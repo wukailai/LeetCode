2190a044609136a54d67230efa6cea929409f28e
“wukailai” <kailaiwu@126.com>
2020-07-12 16:31:52 +0800

    update

--- src/main/java/array/FindThreeSumZero.java
+++ src/main/java/array/FindThreeSumZero.java
@@ -1,6 +1,7 @@
 package array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -9,11 +10,49 @@ import java.util.List;
 
 public class FindThreeSumZero {
     public static void main(String[] args) {
-
+        int[] arr = new int[] {-1, 0, 1, 2, -1, -4, -1};
+        List<int[]> list = find(arr);
+        for (int[] a : list) {
+            System.out.println(Arrays.toString(a));
+        }
     }
 
+    /**
+     * 先排序
+     */
     private static List<int[]> find(int[] a) {
         List<int[]> list = new ArrayList();
+        Arrays.sort(a);
+        for (int i = 0; i < a.length - 2; i++) {
+            if (a[i] > 0) {
+                break;
+            }
+            if (i > 0 && a[i] == a[i - 1]) {
+                continue;
+            }
+            int target = -a[i];
+            int start = i + 1;
+            int end = a.length - 1;
+            while (start < end) {
+                if (a[start] + a[end] == target) {
+                    list.add(new int[] {a[i], a[start], a[end]});
+                    while (a[start] == a[start + 1]) {
+                        start++;
+                    }
+                    while (a[end] == a[end - 1] && start < end) {
+                        end--;
+                    }
+                    start++;
+                    end--;
+                }
+                while (a[start] + a[end] > target && start < end) {
+                    end--;
+                }
+                while (a[start] + a[end] < target && start < end) {
+                    start++;
+                }
+            }
+        }
         return list;
     }
 }
---
+++ src/main/java/array/FindTwoSumZeroIndexSorted.java
@@ -0,0 +1,50 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 在一个有序数组中找出所有两数之和等于0的下标
+ */
+
+public class FindTwoSumZeroIndexSorted {
+    public static void main(String[] args) {
+        int[] arr = new int[] {-2, -1, -1, 0, 1, 2, 3};
+        List<int[]> list = find(arr);
+        for (int[] a : list) {
+            System.out.println(Arrays.toString(a));
+        }
+    }
+
+    private static List<int[]> find(int[] a) {
+        List<int[]> list = new ArrayList();
+        int start = 0;
+        int end = a.length - 1;
+        while (start < end) {
+            if (a[start] > 0) {
+                break;
+            }
+            if (a[start] + a[end] == 0) {
+                list.add(new int[] {start, end});
+                while (a[end] == a[end - 1]) {
+                    list.add(new int[] {start, end - 1});
+                    end--;
+                }
+                while (a[start] == a[start + 1]) {
+                    list.add(new int[] {start + 1, end});
+                    start++;
+                }
+                start++;
+                end--;
+            }
+            while (a[start] + a[end] > 0 && start < end) {
+                end--;
+            }
+            while (a[start] + a[end] < 0 && start < end) {
+                start++;
+            }
+        }
+        return list;
+    }
+}
