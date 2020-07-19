91ba8f92d5bf8099d74cb791289e62ffb6d0d63e
“wukailai” <kailaiwu@126.com>
2020-07-19 23:18:56 +0800

    update

--- src/main/java/array/FindAllSubSets.java
+++ src/main/java/array/FindAllSubSets.java
@@ -13,21 +13,21 @@ public class FindAllSubSets {
     public static void main(String[] args) {
         int[] arr = new int[] {1, 2, 2};
         Arrays.sort(arr);
-        findAllSubSets(0, arr.length, arr, new ArrayList());
+        findAllSubSets(0, arr, new ArrayList());
         System.out.println(result);
     }
 
-    private static void findAllSubSets(int index, int len, int[] a, List<Integer> trace) {
-        if (index == len) {
+    private static void findAllSubSets(int index, int[] a, List<Integer> trace) {
+        if (index == a.length) {
             return;
         }
-        for (int j = index; j < len; j++) {
+        for (int j = index; j < a.length; j++) {
             if (j > index && a[j - 1] == a[j]) {
                 continue;
             }
             trace.add(a[index]);
             result.add(new ArrayList(trace));
-            findAllSubSets(index + 1, len, a, trace);
+            findAllSubSets(index + 1, a, trace);
             trace.remove(trace.size() - 1);
         }
     }
