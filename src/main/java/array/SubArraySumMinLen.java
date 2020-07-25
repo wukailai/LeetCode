bd26f31dce8e9e0b8c117851e5afadfaaccd73db
“wukailai” <kailaiwu@126.com>
2020-07-25 23:28:02 +0800

    update

---
+++ src/main/java/array/SubArraySumMinLen.java
@@ -0,0 +1,33 @@
+package array;
+
+/**
+ * 子数组之和大于给定值的最小长度
+ */
+
+public class SubArraySumMinLen {
+    public static void main(String[] args) {
+        int[] arr = new int[] {2, 3, 1, 2, 4, 3};
+        int target = 7;
+        System.out.println(find(arr, target));
+    }
+
+    /**
+     * 双指针解法
+     */
+    private static int find(int[] a, int target) {
+        int result = Integer.MAX_VALUE;
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+        while (end < a.length) {
+            sum += a[end];
+            while (sum >= target) {
+                result = Math.min(result, end - start + 1);
+                sum -= a[start];
+                start++;
+            }
+            end++;
+        }
+        return result;
+    }
+}
