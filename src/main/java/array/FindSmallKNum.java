e59ce80c00f8402c636a649b73c4ca3343632c5c
“wukailai” <kailaiwu@126.com>
2020-07-26 15:52:42 +0800

    update

--- src/main/java/array/FindSmallKNum.java
+++ src/main/java/array/FindSmallKNum.java
@@ -6,40 +6,42 @@ import util.GeneratorUtil;
 import java.util.Arrays;
 
 /**
- * 找出数组中最大的K个值
+ * 找出数组中最小的K个值
  */
 
-public class FindLargestKNum {
+public class FindSmallKNum {
     public static void main(String[] args) {
         int[] arr = GeneratorUtil.newIntArray();
         int k = 3;
-        int[] result = getLLargestK(arr, k);
+        int[] result = getSmallestK(arr, k);
         System.out.println(Arrays.toString(result));
     }
 
-    private static int[] getLLargestK(int[] a, int k) {
-        // 构建小顶堆
+    private static int[] getSmallestK(int[] a, int k) {
+        //构建大顶堆
         for (int i = k / 2 - 1; i >= 0; i--) {
             adjustHeap(a, i, k);
         }
         for (int i = k; i < a.length; i++) {
-            if (a[i] <= a[0]) {
+            if (a[i] >= a[0]) {
                 continue;
             }
-            //替换
             a[0] = a[i];
-            //调整堆
+            //调整大顶堆
             adjustHeap(a, 0, k);
         }
         return Arrays.copyOfRange(a, 0, k);
     }
 
+    /**
+     * 调整大顶堆
+     */
     private static void adjustHeap(int[] a, int i, int len) {
-        for (int j = 2 * i + 1; j < len; j = 2 * j + 1) {
-            if (a[j + 1] < a[j] && j + 1 < a.length) {
+        for (int j = 2 * i + 1; j < len; j++) {
+            if (a[j] < a[j + 1] && j + 1 < len) {
                 j++;
             }
-            if (a[i] <= a[j]) {
+            if (a[i] >= a[j]) {
                 break;
             }
             CommonUtil.swapIntArray(a, i, j);
