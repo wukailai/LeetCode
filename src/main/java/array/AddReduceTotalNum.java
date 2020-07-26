c1964538662c93e1628f8b022f7a4440d23eb4e8
“wukailai” <kailaiwu@126.com>
2020-07-26 22:26:55 +0800

    update

--- src/main/java/array/AddReduceTotalNum.java
+++ src/main/java/array/AddReduceTotalNum.java
@@ -1,22 +1,30 @@
 package array;
 
+/**
+ * 数组添加正负号使得计算之和等于目标值的次数
+ */
+
 public class AddReduceTotalNum {
     private static int count = 0;
 
     public static void main(String[] args) {
         int[] arr = new int[] {1, 1, 1, 1, 1};
-        calculate(arr, 0, 0, 3);
+        int target = 3;
+        calculate(arr, 0, 0, target);
         System.out.println(count);
     }
 
-    private static void calculate(int[] a, int i, int cur, int target) {
+    /**
+     * 数组之和（可正可负）等于target
+     */
+    private static void calculate(int[] a, int i, int curTotal, int target) {
         if (i == a.length) {
-            if (cur == target) {
+            if (curTotal == target) {
                 count++;
             }
         } else {
-            calculate(a, i + 1, cur + a[i], target);
-            calculate(a, i + 1, cur - a[i], target);
+            calculate(a, i + 1, curTotal + a[i], target);
+            calculate(a, i + 1, curTotal - a[i], target);
         }
     }
 }
