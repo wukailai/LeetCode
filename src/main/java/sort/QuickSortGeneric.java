65a089539c9abc910efcdefe2a37faa5629fa4dd
“wukailai” <kailaiwu@126.com>
2021-03-06 11:08:49 +0800

    update

--- src/main/java/sort/QuickSortGeneric.java
+++ src/main/java/sort/QuickSortGeneric.java
@@ -1,8 +1,10 @@
 package sort;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 快排-范型实现
@@ -16,23 +18,26 @@ public class QuickSortGeneric {
         list.add(2);
         quickSort(list, 0, list.size() - 1);
         System.out.println(list);
+
+        Character[] cArray = new Character[] {'c', 'a', 'a', 'd', 'b'};
+        System.out.println(Arrays.toString(cArray));
+        quickSort2(cArray);
+        System.out.println(Arrays.toString(cArray));
     }
 
     private static <T extends Comparable> void quickSort(List<T> list, int start, int end) {
-        T temp = list.get(0);
         int i = start;
         int j = end;
         while (i < j) {
-            while (isBiggerAndEqual(list.get(j), temp) && i < j) {
+            while (i < j && list.get(j).compareTo(list.get(0)) >= 0) {
                 j--;
             }
-            while (isBiggerAndEqual(temp, list.get(i)) && i < j) {
+            while (i < j && list.get(i).compareTo(list.get(0)) <= 0) {
                 i++;
             }
             Collections.swap(list, i, j);
         }
-        list.set(start, list.get(i));
-        list.set(i, temp);
+        Collections.swap(list, 0, i);
         if (i - 1 > start) {
           quickSort(list, start, i - 1);
         }
@@ -41,7 +46,44 @@ public class QuickSortGeneric {
         }
     }
 
-    public static <T extends Comparable> boolean isBiggerAndEqual(T a, T b) {
-        return a.compareTo(b) >= 0;
+    private static <T extends Comparable> void quickSort2(T[] a) {
+        Stack<Integer> stack = new Stack();
+        stack.push(0);
+        stack.push(a.length - 1);
+        while (!stack.isEmpty()) {
+            int j = stack.pop();
+            int i = stack.pop();
+            int index = getIndex(a, i, j);
+            if (i < index - 1) {
+                stack.push(i);
+                stack.push(index - 1);
+            }
+            if (index + 1 < j) {
+                stack.push(index + 1);
+                stack.push(j);
+            }
+        }
+    }
+
+    private static <T extends Comparable> int getIndex(T[] a, int start, int end) {
+        int i = start;
+        int j = end;
+        while (i < j) {
+            while (i < j && a[j].compareTo(a[start]) >= 0) {
+                j--;
+            }
+            while (i <j && a[i].compareTo(a[start]) <= 0) {
+                i++;
+            }
+            swap(a, i, j);
+        }
+        swap(a, start, i);
+        return i;
+    }
+
+    private static <T extends Comparable> void swap(T[] a, int i, int j) {
+        T temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
     }
 }
