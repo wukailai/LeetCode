b6b8bc648f0c828d48cb500ed8f0991136b3715c
“wukailai” <kailaiwu@126.com>
2021-03-06 22:45:57 +0800

    update

--- src/main/java/string/IsPalindrome.java
+++ src/main/java/string/IsPalindrome.java
@@ -8,9 +8,24 @@ public class IsPalindrome {
     public static void main(String[] args) {
         System.out.println(isPalindrome("12321"));
         System.out.println(isPalindrome("123456"));
+        System.out.println(isPalindrome2("12321"));
+        System.out.println(isPalindrome2("123456"));
     }
 
     private static boolean isPalindrome(String s) {
         return s.equals(new StringBuilder(s).reverse().toString());
+    }
+
+    private static boolean isPalindrome2(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
     }
 }
