871b9d97e78a3ebf031a339ff3e17ce779b2f6ba
“wukailai” <kailaiwu@126.com>
2021-03-06 18:07:49 +0800

    update

--- src/main/java/thread/FindFastestTranslator.java
+++ src/main/java/thread/FindFastestTranslator.java
@@ -1,8 +1,11 @@
 package thread;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
@@ -10,64 +13,100 @@ import java.util.concurrent.atomic.AtomicReference;
  */
 
 public class FindFastestTranslator {
-    private static AtomicReference<String> result = new AtomicReference(null);
+    private Random random = new Random();
+    private AtomicReference<String> result = new AtomicReference();
+    private ExecutorService es = Executors.newFixedThreadPool(3);
+    private Map<String, Future> futureMap = new HashMap();
 
     public static void main(String[] args) {
-        ExecutorService es = Executors.newFixedThreadPool(2);
-        es.submit(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    result.compareAndSet(null, baiduTrans());
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        FindFastestTranslator findFastestTranslator = new FindFastestTranslator();
+        findFastestTranslator.translate();
+    }
+
+    private String translate() {
+        long startTime = System.currentTimeMillis();
+        Future googleFuture = es.submit(() -> {
+            try {
+                if (result.compareAndSet(null, googleTranslate())) {
+                    stopOthers();
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         });
-        es.submit(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    result.compareAndSet(null, googleTrans());
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        Future baiduFuture = es.submit(() -> {
+            try {
+                if (result.compareAndSet(null, baiduTranslate())) {
+                    stopOthers();
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         });
-        es.submit(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    result.compareAndSet(null, wangyiTrans());
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        Future netEaseFuture = es.submit(() -> {
+            try {
+                if (result.compareAndSet(null, netEaseTranslate())) {
+                    stopOthers();
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         });
+        futureMap.put("Google", googleFuture);
+        futureMap.put("Baidu", baiduFuture);
+        futureMap.put("NetEase", netEaseFuture);
         while (null == result.get()) {
-            System.out.println("----等待结果-----");
+            //TODO
         }
-        System.out.println(result.get());
+        System.out.println("Combine Translator cost time: " + (System.currentTimeMillis() - startTime));
+        return result.get();
     }
 
-    private static String baiduTrans() throws InterruptedException {
-        int sleepTime = new Random().nextInt(10);
-        Thread.sleep(sleepTime * 1000);
-        System.out.println("baidu执行了" + sleepTime + "秒");
-        return "baidu";
+    private void stopOthers() {
+        for (Map.Entry<String, Future> entry : futureMap.entrySet()) {
+            String key = entry.getKey();
+            Future future = entry.getValue();
+            if (result.get().equals(key)) {
+                continue;
+            }
+            future.cancel(true);
+        }
+        futureMap.clear();
     }
 
-    private static String googleTrans() throws InterruptedException {
-        int sleepTime = new Random().nextInt(10);
-        Thread.sleep(sleepTime * 1000);
-        System.out.println("google执行了" + sleepTime + "秒");
-        return "google";
+    private String googleTranslate() {
+        long costTime = random.nextInt(5000);
+        try {
+            Thread.sleep(costTime);
+        } catch (InterruptedException e) {
+            System.out.println("Google translator has been stopped in advance");
+            return null;
+        }
+        System.out.println("Google translator cost time: " + costTime);
+        return "Google";
     }
 
-    private static String wangyiTrans() throws InterruptedException {
-        int sleepTime = new Random().nextInt(10);
-        Thread.sleep(3000);
-        System.out.println("wangyi执行了" + sleepTime + "秒");
-        return "wangyi";
+    private String baiduTranslate() {
+        long costTime = random.nextInt(5000);
+        try {
+            Thread.sleep(costTime);
+        } catch (InterruptedException e) {
+            System.out.println("Baidu translator has been stopped in advance");
+            return null;
+        }
+        System.out.println("Baidu translator cost time: " + costTime);
+        return "Baidu";
+    }
+
+    private String netEaseTranslate() {
+        long costTime = random.nextInt(5000);
+        try {
+            Thread.sleep(costTime);
+        } catch (InterruptedException e) {
+            System.out.println("NetEase translator has been stopped in advance");
+            return null;
+        }
+        System.out.println("NetEase translator cost time: " + costTime);
+        return "NetEase";
     }
 }
