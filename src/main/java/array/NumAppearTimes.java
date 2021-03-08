4322a862bd714c94297c02e778981fe262a2bfba
wukailai1 <wukailai1@xiaomi.com>
2021-03-08 10:28:05 +0800

    update

--- src/main/java/array/NumAppearTimes.java
+++ src/main/java/array/NumAppearTimes.java
@@ -7,7 +7,7 @@ package array;
 public class NumAppearTimes {
     public static void main(String[] args) {
         int[] arr = new int[] {1, 1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 6, 6, 6, 6};
-        int target = 6;
+        int target = 3;
         System.out.println(getTimes(arr, target));
         System.out.println(getTimes2(arr, target));
     }
@@ -22,53 +22,38 @@ public class NumAppearTimes {
         if (a[0] > target || a[a.length - 1] < target) {
             return 0;
         }
-        int index = getIndex(a, 0, a.length - 1, target);
-        if (index == -1) {
-            return 0;
-        }
-        int left = getLeft(a, 0, index, target);
-        int right = getRight(a, index, a.length - 1, target);
+        int left = getLeft(a, target);
+        int right = getRight(a, target);
         return right - left + 1;
     }
 
-    private static int getIndex(int[] a, int start, int end, int target) {
+    private static int getLeft(int[] a, int target) {
+        int start = 0;
+        int end = a.length - 1;
         while (start <= end) {
             int mid = (end - start) / 2 + start;
-            if (a[mid] == target) {
-                return mid;
-            }
-            if (a[mid] > target) {
+            if (a[mid] >= target) {
                 end = mid - 1;
-            } else {
-                start = mid + 1;
             }
-        }
-        return -1;
-    }
-
-    private static int getLeft(int[] a, int start, int end, int target) {
-        while (start < end) {
-            int mid = (end - start) / 2 + start;
-            if (a[mid] == target) {
-                end = mid;
-            } else {
+            else {
                 start = mid + 1;
-            }
-        }
-        return end;
-    }
-
-    private static int getRight(int[] a, int start, int end, int target) {
-        while (start < end) {
-            //这个地方为什么+1无法理解
-            int mid = (end + 1 - start) / 2 + start;
-            if (a[mid] == target) {
-                start = mid;
-            } else {
-                end = mid - 1;
             }
         }
         return start;
+    }
+
+    private static int getRight(int[] a, int target) {
+        int start = 0;
+        int end = a.length - 1;
+        while (start <= end) {
+            int mid = (end - start) / 2 + start;
+            if (a[mid] <= target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return end;
     }
 
     /**
