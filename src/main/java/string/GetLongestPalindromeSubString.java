ba3485570dfc00c8a7a203cf76c8153cc0ca0bd6
wukailai1 <wukailai1@xiaomi.com>
2021-03-08 11:23:26 +0800

    update

--- src/main/java/string/GetLongestPalindromeSubString.java
+++ src/main/java/string/GetLongestPalindromeSubString.java
@@ -33,4 +33,27 @@ public class GetLongestPalindromeSubString {
         }
         return result;
     }
+
+    /**
+     * 动态规划2，更容易理解
+     */
+    private static String getSubString2(String s) {
+        String result = "";
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        for (int len = 1; len <= s.length(); len++) {
+            for (int start = 0; start < s.length(); start++) {
+                int end = start + len - 1;
+                if (end > s.length() - 1) {
+                    break;
+                }
+                if (len == 1 || (s.charAt(start) == s.charAt(end) && dp[start + 1][end - 1])) {
+                    dp[start][end] = true;
+                }
+                if (dp[start][end] && end - start + 1 > result.length()) {
+                    result = s.substring(start, end + 1);
+                }
+            }
+        }
+        return result;
+    }
 }
