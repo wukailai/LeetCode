a7a9d90ca047497a6d995441207bfb614c10dcee
wukailai1 <wukailai1@xiaomi.com>
2021-03-18 22:02:43 +0800

    update

---
+++ src/main/java/tree/BinaryTreeDiameter.java
@@ -0,0 +1,41 @@
+package tree;
+
+import util.BinaryNode;
+import util.GeneratorUtil;
+
+/**
+ * 二叉树的直径（直径比节点数小1）
+ */
+
+public class BinaryTreeDiameter {
+    public static void main(String[] args) {
+        BinaryNode root = GeneratorUtil.newBinary();
+        System.out.println(getDiameter(root));
+    }
+
+    /**
+     * 三种情况：
+     * 1）直径包含根节点，直径=左子树高度 + 右子树高度 + 1；
+     * 2）左子树直径；
+     * 3）右子树直径；
+     */
+    private static int getDiameter(BinaryNode root) {
+        int diameter = 0;
+        if (null == root) {
+            return 0;
+        }
+        diameter = Math.max(diameter, getHeight(root.getLeft()) + getHeight(root.getRight()));
+        diameter = Math.max(diameter, getDiameter(root.getLeft()));
+        diameter = Math.max(diameter, getDiameter(root.getRight()));
+        return diameter;
+    }
+
+    private static int getHeight(BinaryNode root) {
+        if (null == root) {
+            return 0;
+        }
+        int leftHeight = getHeight(root.getLeft());
+        int rightHeight = getHeight(root.getRight());
+        return (leftHeight > rightHeight ? leftHeight : rightHeight) + 1;
+    }
+}
---
+++ src/main/java/tree/MergeTwoBinaryTree.java
@@ -0,0 +1,32 @@
+package tree;
+
+import util.BinaryNode;
+import util.GeneratorUtil;
+
+/**
+ * 合并两个二叉树
+ */
+
+public class MergeTwoBinaryTree {
+    public static void main(String[] args) {
+        BinaryNode root1 = GeneratorUtil.newBinary();
+        BinaryNode root2 = GeneratorUtil.newBinary();
+        BinaryNode root = merge(root1, root2);
+        PrintBinaryTree.layerPrint(root1);
+        PrintBinaryTree.layerPrint(root2);
+        PrintBinaryTree.layerPrint(root);
+    }
+
+    private static BinaryNode merge(BinaryNode root1, BinaryNode root2) {
+        if (null == root1) {
+            return root2;
+        }
+        if (null == root2) {
+            return root1;
+        }
+        BinaryNode newBinaryNode = new BinaryNode(root1.getVal() + root2.getVal());
+        newBinaryNode.setLeft(merge(root1.getLeft(), root2.getLeft()));
+        newBinaryNode.setRight(merge(root1.getRight(), root2.getRight()));
+        return newBinaryNode;
+    }
+}
