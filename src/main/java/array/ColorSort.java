7e4033479abff384418496f7bd0a544d3877f36b
“wukailai” <kailaiwu@126.com>
2021-03-20 21:29:22 +0800

    update

--- src/main/java/array/ColorSort.java
+++ src/main/java/array/ColorSort.java
@@ -11,7 +11,8 @@ import java.util.Arrays;
 public class ColorSort {
     public static void main(String[] args) {
         int[] arr = new int[] {0, 1, 2, 0, 1, 2, 0, 0, 2, 2, 1};
-        sort(arr);
+        //sort(arr);
+        sort2(arr);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -28,6 +29,26 @@ public class ColorSort {
             if (a[i] == 2 && i != blue) {
                 CommonUtil.swapIntArray(a, i, blue);
                 blue--;
+            }
+        }
+    }
+
+    private static void sort2(int[] a) {
+        int red = 0;
+        int blue = a.length - 1;
+        int i = 0;
+        while (i <= blue) {
+            if (a[i] == 0) {
+                CommonUtil.swapIntArray(a, i, red);
+                // 这里i可以往前走是因为从左边换过来的数已经看见了
+                red++;
+                i++;
+            } else if (a[i] == 2) {
+                CommonUtil.swapIntArray(a, i, blue);
+                // 这里i不能往前走是因为从右边换过来的数不可见
+                blue--;
+            } else {
+                i++;
             }
         }
     }
