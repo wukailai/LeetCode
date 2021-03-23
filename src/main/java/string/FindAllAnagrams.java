2bc992276426115f80e08cf9f1c70e7d24b6e145
“wukailai” <kailaiwu@126.com>
2021-03-23 19:36:27 +0800

    update

--- src/main/java/string/FindAllAnagrams.java
+++ src/main/java/string/FindAllAnagrams.java
@@ -19,14 +19,14 @@ public class FindAllAnagrams {
         for (int right = 1; right < s.length(); right++) {
            if (!isInit) {
                while (right - left + 1 != n.length()) {
-                    cur += s.charAt(right);
-                    right++;
-                }
+                   cur += s.charAt(right);
+                   right++;
+               }
                cur += s.charAt(right);
-                isInit = true;
+               isInit = true;
             } else {
-                cur = cur.substring(1) + s.charAt(right);
-                left++;
+               cur = cur.substring(1) + s.charAt(right);
+               left++;
             }
             if (IsSameIgnoreOrder.isSame(n, cur)) {
                 result.add(left);
