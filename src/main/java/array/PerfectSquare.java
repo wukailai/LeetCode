f019076462d51a2a479138b1e8d39d66cc078827
wukailai1 <wukailai1@xiaomi.com>
2021-03-30 17:57:11 +0800

    update

---
+++ src/main/java/array/PerfectSquare.java
@@ -0,0 +1,29 @@
+package array;
+
+/**
+ * 找出n需要最少的平方数个数
+ */
+
+public class PerfectSquare {
+    public static void main(String[] args) {
+        System.out.println(find(12));
+        System.out.println(find(13));
+    }
+
+    /**
+     * 动态规划
+     */
+    private static int find(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        for (int i = 1; i < n + 1; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            for (int j = 1; j <= n; j++) {
+                if (i - j * j >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+                }
+            }
+        }
+        return dp[n];
+    }
+}
