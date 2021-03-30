42c17d5306f4ad89a412a0f4ddb26b307b504b1d
wukailai1 <wukailai1@xiaomi.com>
2021-03-30 20:47:29 +0800

    update

---
+++ src/main/java/tree/HouseRobber3.java
@@ -0,0 +1,27 @@
+package tree;
+
+import util.BinaryNode;
+
+/**
+ * house robber III
+ */
+
+public class HouseRobber3 {
+    public static void main(String[] args) {
+    }
+
+    private static int get(BinaryNode root) {
+        if (null == root) {
+            return 0;
+        }
+        int result = root.getVal();
+        if (root.getLeft() != null) {
+            result += get(root.getLeft().getLeft()) + get(root.getLeft().getRight());
+        }
+        if (root.getRight() != null) {
+            result += get(root.getRight().getLeft()) + get(root.getRight().getRight());
+        }
+        result = Math.max(result, get(root.getLeft()) + get(root.getRight()));
+        return result;
+    }
+}
