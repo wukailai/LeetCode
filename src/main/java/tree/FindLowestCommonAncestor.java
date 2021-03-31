1e5e46ad6ae2ed4cb474884cc2ef11b8beecc6eb
wukailai1 <wukailai1@xiaomi.com>
2021-03-31 21:11:26 +0800

    update

---
+++ src/main/java/tree/FindLowestCommonAncestor.java
@@ -0,0 +1,34 @@
+package tree;
+
+import util.BinaryNode;
+import util.GeneratorUtil;
+
+/**
+ * 找到最低共有祖先
+ */
+
+public class FindLowestCommonAncestor {
+    public static void main(String[] args) {
+        BinaryNode root = GeneratorUtil.newBinary2();
+        BinaryNode p = root.getLeft().getLeft();
+        // BinaryNode q = root.getLeft().getRight();
+        BinaryNode q = root.getRight().getRight();
+        BinaryNode node = find(root, p, q);
+        System.out.println(node.getVal());
+    }
+
+    private static BinaryNode find(BinaryNode root, BinaryNode p, BinaryNode q) {
+        if (null == root || root == p || root == q) {
+            return root;
+        }
+        BinaryNode left = find(root.getLeft(), p, q);
+        BinaryNode right = find(root.getRight(), p, q);
+        if (null == left) {
+            return right;
+        }
+        if (null == right) {
+            return left;
+        }
+        return root;
+    }
+}
