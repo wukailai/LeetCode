3f2f4c3daf4531bb106b92fc7d3d46f83306d4cd
wukailai1 <wukailai1@xiaomi.com>
2021-03-31 20:52:13 +0800

    update

---
+++ src/main/java/tree/BSTNum.java
@@ -0,0 +1,23 @@
+package tree;
+
+/**
+ * 二叉搜索树总颗树
+ */
+
+public class BSTNum {
+    public static void main(String[] args) {
+        System.out.println(getSum(13));
+    }
+
+    private static int getSum(int n) {
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        result[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                result[i] += result[j - 1] * result[i - j];
+            }
+        }
+        return result[n];
+    }
+}
