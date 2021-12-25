1a5c1f781e390f4dba0cd4cef3e8966b8a014111
“wukailai” <kailaiwu@126.com>
2021-12-25 20:31:28 +0800

    update

--- src/main/java/array/CoinExchange2.java
+++ src/main/java/array/CoinExchange2.java
@@ -9,7 +9,7 @@ public class CoinExchange2 {
     public static void main(String[] args) {
         int[] arr = new int[] {1, 2, 5};
         int target = 5;
-        System.out.println(exchange2(arr, target));
+        System.out.println(exchange3(arr, target));
     }
 
     private static int exchange(int[] a, int target) {
@@ -31,6 +31,17 @@ public class CoinExchange2 {
                 for (int k = 0; k <= j / a[i - 1]; k++) {
                     dp[i][j] += dp[i - 1][j - k * a[i - 1]];
                 }
+            }
+        }
+        return dp[a.length][target];
+    }
+
+    private static int exchange3(int[] a, int target) {
+        int[][] dp = new int[a.length + 1][target + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= a.length; i++) {
+            for (int j = 0; j <= target; j++) {
+                dp[i][j] = dp[i - 1][j] + (j - a[i - 1] >= 0 ? dp[i][j - a[i - 1]] : 0);
             }
         }
         return dp[a.length][target];
