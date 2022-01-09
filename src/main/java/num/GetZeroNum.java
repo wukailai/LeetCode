fcc3cc1800041055464470ac5047138219a9c27b
“wukailai” <kailaiwu@126.com>
2022-01-09 11:06:36 +0800

    update

--- src/main/java/num/GetZeroNum.java
+++ src/main/java/num/GetZeroNum.java
@@ -18,8 +18,8 @@ public class GetZeroNum {
     private static int getZeroNum(int n) {
         int result = 0;
         while (n != 0) {
-            result += n / 5;
             n = n / 5;
+            result += n;
         }
         return result;
     }
