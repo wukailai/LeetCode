70fba80b0b3e24b6944a4156a55c153a0da760e7
“wukailai” <kailaiwu@126.com>
2022-01-09 10:49:54 +0800

    update

--- src/main/java/num/ExchangeBottles.java
+++ src/main/java/num/ExchangeBottles.java
@@ -1,7 +1,7 @@
 package num;
 
 /**
- * 换瓶子：三个空汽水瓶可以换一瓶汽水，如果最后仅剩一个瓶子，可以让老板先借给你一瓶汽水，喝掉这瓶满的，喝完以后用3个空瓶子换一瓶满的还给老板
+ * 换瓶子：三个空汽水瓶可以换一瓶汽水，如果最后剩两个瓶子，可以让老板先借给你一瓶汽水，喝掉这瓶满的，喝完以后用3个空瓶子换一瓶满的还给老板
  * 求最多可以换多少瓶汽水喝
  */
 
@@ -21,6 +21,17 @@ public class ExchangeBottles {
         }
         if (n == 2) {
             result++;
+        }
+        return result;
+    }
+
+    private static int exchange(int n, int m) {
+        int result = 0;
+        while (n >= m) {
+            int remain = n % m;
+            n = n / m;
+            result += n;
+            n += remain;
         }
         return result;
     }
