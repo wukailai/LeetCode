aa99c01e4e4531e5cf231ec81acf0d311dd774bc
“wukailai” <kailaiwu@126.com>
2022-08-21 15:48:55 +0800

    update

--- src/main/java/string/CompressString.java
+++ src/main/java/string/CompressString.java
@@ -8,6 +8,7 @@ public class CompressString {
     public static void main(String[] args) {
         System.out.println(compressString("aaaacffcvbb"));
         System.out.println(compressString("a"));
+        System.out.println(uncompressString("a4cf2cvb2h6"));
     }
 
     private static String compressString(String s) {
@@ -21,7 +22,9 @@ public class CompressString {
             char c = s.charAt(i);
             if (c != cur) {
                 sb.append(cur);
-                sb.append(num);
+                if (num > 1) {
+                    sb.append(num);
+                }
                 cur = c;
                 num = 1;
             } else {
@@ -29,7 +32,48 @@ public class CompressString {
             }
         }
         sb.append(cur);
-        sb.append(num);
+        if (num > 1) {
+            sb.append(num);
+        }
         return sb.toString();
+    }
+
+    private static String uncompressString(String s) {
+        if (null == s || s.length() < 2) {
+            return s;
+        }
+        StringBuilder sb = new StringBuilder();
+        int start = 0;
+        int end = start + 1;
+        while (end < s.length()) {
+            char startCharacter = s.charAt(start);
+            char endCharacter = s.charAt(end);
+            int number = 1;
+            if (isNumber(endCharacter)) {
+                String numberStr = "";
+                while (isNumber(endCharacter)) {
+                    numberStr += endCharacter;
+                    if (end++ == s.length() - 1) {
+                        break;
+                    }
+                    endCharacter = s.charAt(end);
+                }
+                number = Integer.parseInt(numberStr);
+            }
+            int j = 0;
+            while (j++ < number) {
+                sb.append(startCharacter);
+            }
+            start = end;
+            end = start + 1;
+        }
+        if (start ==  s.length() - 1) {
+            sb.append(s.charAt(start));
+        }
+        return sb.toString();
+    }
+
+    private static boolean isNumber(char c) {
+        return c >= '0' && c <= '9';
     }
 }
